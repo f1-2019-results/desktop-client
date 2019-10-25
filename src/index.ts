19e789ec9280f92517986e5d2f84088c47ac66ea
Jesse Niininen <findarkside@gmail.com>
2019-10-25 22:44:58 +0300

    Implement waiting for event start and end

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,38 @@ import * as dgram from 'dgram';
 import * as fs from 'fs';
 const port = 20777;
 
+const HEADER_SIZE = 24;
+
 async function init(): Promise<void> {
     const socket = dgram.createSocket('udp4');
     const writeStream = fs.createWriteStream('./dump.bin');
 
+    let raceStarted = false;
+
     socket.on('message', (buf) => {
         const header = parseHeader(buf);
         if (header.packetId === 3) {
-            writeStream.write(buf);
+            const event = buf.toString('utf8', HEADER_SIZE - 1, HEADER_SIZE + 3);
+            if (event === 'SSTA') {
+                console.log('Race started');
+                raceStarted = true;
+            } else if (event === 'SEND') {
+                console.log('Race ended');
+                raceStarted = false;
+                writeStream.on('close', () => {
+                    process.exit(0);
+                });
+                writeStream.close();
+            }
         }
         if ([1, 2, 3, 4].includes(header.packetId)) {
-            writeStream.write(buf);
+            if (raceStarted)
+                writeStream.write(buf);
         }
     });
 
     socket.on('listening', () => {
-        console.log('Listening to socket');
+        console.log('Waiting for race start');
     });
 
     socket.bind(port)
