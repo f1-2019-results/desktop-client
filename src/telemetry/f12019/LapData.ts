5e59c5e831a81b152c8638e8b98b24b04d7eee51
Jesse Niininen <findarkside@gmail.com>
2019-12-07 17:34:27 +0200

    test linting

--- src/telemetry/f12019/LapData.ts
+++ src/telemetry/f12019/LapData.ts
@@ -2,7 +2,7 @@ export default interface LapData {
     lastLapTime: number
     currentLapTime: number
     sector1: number
-    sector2: number
+            sector2: number
     position: number
     lapNum: number
     currentSector: number
