064ece20249a706ad85e2dd106df70c7f391b0d8
Jesse Niininen <findarkside@gmail.com>
2019-12-14 22:35:26 +0200

    Add more strict linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,23 @@
+const path = require('path');
+
 module.exports = {
     parser: '@typescript-eslint/parser',
+    plugins: ["@typescript-eslint"],
     extends: [
-        'plugin:@typescript-eslint/recommended'
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
     ],
     parserOptions: {
         ecmaVersion: 2018,
-        sourceType: 'module'
+        sourceType: 'module',
+        project: path.resolve(__dirname, './tsconfig.json'),
+        tsconfigRootDir: __dirname,
     },
     rules: {
         '@typescript-eslint/no-use-before-define': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-        'quotes': ['error', 'single']
+        'quotes': ['error', 'single'],
     }
 };
