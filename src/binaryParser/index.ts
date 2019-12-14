33ee8a2f8bfb8c0c71802f2ecb42eabd74f9119d
Jesse Niininen <findarkside@gmail.com>
2019-12-14 21:53:46 +0200

    Refactor binary parser export

--- src/binaryParser/index.ts
+++ src/binaryParser/index.ts
@@ -11,13 +11,13 @@ function createFactoryFunction<A extends any[], R>(constructor: new (...args: A)
 export default {
   object: createFactoryFunction(ObjectParser),
   array: createFactoryFunction(ArrayParser),
-  int8: (...args: ConstructorParameters<typeof parsers.Int8Parser>) => new parsers.Int8Parser(...args),
-  uint8: (...args: ConstructorParameters<typeof parsers.Uint8Parser>) => new parsers.Uint8Parser(...args),
-  int16: (...args: ConstructorParameters<typeof parsers.Int16Parser>) => new parsers.Int16Parser(...args),
-  uint16: (...args: ConstructorParameters<typeof parsers.Uint16Parser>) => new parsers.Uint16Parser(...args),
-  int32: (...args: ConstructorParameters<typeof parsers.Int32Parser>) => new parsers.Int32Parser(...args),
-  uint32: (...args: ConstructorParameters<typeof parsers.Uint32Parser>) => new parsers.Uint32Parser(...args),
-  int64: (...args: ConstructorParameters<typeof parsers.Int64Parser>) => new parsers.Int64Parser(...args),
-  uint64: (...args: ConstructorParameters<typeof parsers.Uint64Parser>) => new parsers.Uint64Parser(...args),
-  string: (...args: ConstructorParameters<typeof parsers.StringParser>) => new parsers.StringParser(...args)
+  int8: createFactoryFunction(parsers.Int8Parser),
+  uint8: createFactoryFunction(parsers.Uint8Parser),
+  int16: createFactoryFunction(parsers.Int16Parser),
+  uint16: createFactoryFunction(parsers.Uint16Parser),
+  int32: createFactoryFunction(parsers.Int32Parser),
+  uint32: createFactoryFunction(parsers.Uint32Parser),
+  int64: createFactoryFunction(parsers.Int64Parser),
+  uint64: createFactoryFunction(parsers.Uint64Parser),
+  string: createFactoryFunction(parsers.StringParser)
 };
