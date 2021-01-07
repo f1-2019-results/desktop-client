7e4876d7b60ff5e1466facc9ccf3b6d5880265e8
Jesse <jesseniininen@gmail.com>
2021-01-07 16:25:28 +0200

    Parse telemetry to new (backend supported) format

--- src/telemetry/f12019/parseDump.ts
+++ src/telemetry/f12019/parseDump.ts
@@ -1,68 +1,68 @@
 import { RaceData } from '../../raceData';
+import { NewRaceBody } from '../../services/raceService';
 import PacketId from './packetId';
 import * as packets from './packets';
+import { RecursivePartial } from '../../util/RecursivePartial';
 
 const packetSizes = [
     1343, 149, 843, 32, 1104, 843, 1347, 1143
 ];
 
-export default function parseDump(buf: Buffer): RaceData {
+export function parseDump(buf: Buffer): NewRaceBody {
     let offset = 0;
-    const result: RaceData = {
-        lapData: [],
-        participants: []
+    const race: RecursivePartial<NewRaceBody> = {
+        game: 'F1 2019',
+        results: [],
     };
-
-    const lastLap = new Array(20).fill(0);
+    const lastLapNum = new Array(20).fill(0);
 
     while (offset < buf.length) {
         const header = packets.Header.parse(buf, offset);
-        if (header.packetId === PacketId.LapData) {
+
+        // TODO: Participants packet is expected to come before LapData packet, check if this is always the case.
+        if (header.packetId === PacketId.Participants) {
+            if (!race.results) {
+                const participants = packets.Participants.parse(buf, offset + packets.Header.size).participants;
+                race.results = participants.map(p => ({
+                    driverId: p.driverId.toString(),
+                    driverName: p.name,
+                    isAi: p.aiControlled,
+                    laps: [],
+                }))
+            }
+        } else if (header.packetId === PacketId.LapData) {
             const lapData = packets.Lap.parse(buf, offset + packets.Header.size);
             for (let i = 0; i < lapData.length; i++) {
                 const data = lapData[i];
-                if (!result.lapData[data.currentLapNum - 1]) {
-                    result.lapData.push({
-                        lap: data.currentLapNum,
-                        timings: [],
-                    });
+                const laps = (race.results as NewRaceBody['results'])[i].laps;
+                if (laps.length < data.currentLapNum) {
+                    laps.push({
+                        position: -1,
+                        sectors: [],
+                        invalid: false,
+                    })
                 }
-                if (!result.lapData[data.currentLapNum - 1].timings[i]) {
-                    result.lapData[data.currentLapNum - 1].timings[i] = {
-                        driverId: result.participants[i].driverId,
-                        invalidLap: false,
-                    };
+                // Fill sector times and position when lap has changed
+                if (data.currentLapNum > 1 && lastLapNum[i] < data.currentLapNum) {
+                    const lastLap = laps[data.currentLapNum - 2];
+                    const sectorTimeSum = lastLap.sectors.reduce((a, b) => a + b);
+                    // Telemetry only contains fields for sector 1 and sector 2 times. 
+                    lastLap.sectors.push(data.lastLapTime - sectorTimeSum);
+                    lastLap.position = data.carPosition;
+                    lastLapNum[i] = data.currentLapNum;
                 }
-                if (data.currentLapNum > 1 && lastLap[i] < data.currentLapNum) {
-                    const currentDriversLapResult = result.lapData[data.currentLapNum - 2].timings[i];
-                    currentDriversLapResult.lapTime = data.lastLapTime;
-                    currentDriversLapResult.position = data.carPosition;
-                    lastLap[i] = data.currentLapNum;
-                }
-                const currentDriversLapResult = result.lapData[data.currentLapNum - 1].timings[i];
-                currentDriversLapResult.driverId = result.participants[i].driverId;
-                if (data.sector1Time > 1)
-                    currentDriversLapResult.sector1 = data.sector1Time;
-                if (data.sector2Time > 2)
-                    currentDriversLapResult.sector2 = data.sector2Time;
+                const currentLap = laps[data.currentLapNum - 1];
+                if (data.sector > 1)
+                    currentLap.sectors[0] = data.sector1Time;
+                if (data.sector > 2)
+                    currentLap.sectors[1] = data.sector2Time;
                 if (data.currentLapInvalid)
-                    currentDriversLapResult.invalidLap = true;
-            }
-        } else if (header.packetId === PacketId.Participants) {
-            if (!result.participants.length) {
-                result.participants = packets.Participants.parse(buf, offset + packets.Header.size).participants;
-                // Fill driver ids in case participants message is after first lap has finished (shouldn't be possible)
-                if (result.lapData) {
-                    for (const lapData of result.lapData) {
-                        for (let i = 0; i < lapData.timings.length; i++) {
-                            lapData.timings[i].driverId = result.participants[i].driverId;
-                        }
-                    }
-                }
+                    currentLap.invalid = true;
             }
         }
         offset += packetSizes[header.packetId];
     }
-    return result;
+
+    return race as NewRaceBody;
 }
 
