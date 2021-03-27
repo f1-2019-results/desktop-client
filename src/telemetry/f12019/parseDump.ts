6a346d292af01f92971c47d6912205abc23e247e
Jesse <jesseniininen@gmail.com>
2021-03-27 12:36:44 +0200

    Ignore cooldown lap after race

--- src/telemetry/f12019/parseDump.ts
+++ src/telemetry/f12019/parseDump.ts
@@ -16,6 +16,8 @@ export default function parseDump(buf: Buffer): NewRaceBody {
         startTime: new Date().toISOString(),
     };
     const lastLapNum = new Array(20).fill(0);
+    // This will be changed to real number before second lap packets come in
+    let totalLaps = 1;
 
     while (offset < buf.length) {
         const header = packets.Header.parse(buf, offset);
@@ -34,6 +36,7 @@ export default function parseDump(buf: Buffer): NewRaceBody {
         } else if (header.packetId === PacketId.Session) {
             const session = packets.Session.parse(buf, offset + packets.Header.size);
             race.trackId = session.trackId.toString();
+            totalLaps = session.totalLaps;
         } else if (header.packetId === PacketId.LapData) {
             const lapData = packets.Lap.parse(buf, offset + packets.Header.size);
             for (let i = 0; i < lapData.length; i++) {
@@ -41,9 +44,12 @@ export default function parseDump(buf: Buffer): NewRaceBody {
                 const result = (race.results as NewRaceBody['results'])[i];
                 const laps = result.laps;
 
+
                 if (!result.startPosition)
                     result.startPosition = data.gridPosition;
-                if (laps.length < data.currentLapNum) {
+
+                // Don't add new lap if driver has finished race
+                if (laps.length < data.currentLapNum && data.currentLapNum <= totalLaps) {
                     laps.push({
                         position: -1,
                         sectors: [],
